0678294ae073bffeaa98c1640294563ebbfe235b
zihang dai <zander.dai@gmail.com>
2016-06-10 19:17:15 -0400

    typoe

--- tensorflow/ptb/run_ptb_med.sh
+++ tensorflow/ptb/run_ptb_med.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -e
-CUDA_VISIBLE_DEVICES=1 python ptb_word_lm.py --data_path=${HOME}/SSD/rnn_benchmark/torch/ptb/lstm/data --model medium
+CUDA_VISIBLE_DEVICES=1 python ptb_word_lm.py --data_path=${HOME}/SSD/rnn_benchmarks/torch/ptb/lstm/data --model medium
--- tensorflow/ptb/run_ptb_small.sh
+++ tensorflow/ptb/run_ptb_small.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -e
-CUDA_VISIBLE_DEVICES=2 python ptb_word_lm.py --data_path=${HOME}/SSD/rnn_benchmark/torch/ptb/lstm/data --model small
+CUDA_VISIBLE_DEVICES=2 python ptb_word_lm.py --data_path=${HOME}/SSD/rnn_benchmarks/torch/ptb/lstm/data --model small
